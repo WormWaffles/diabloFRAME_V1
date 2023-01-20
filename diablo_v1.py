7a0655d9f104cec9919368bb6a048f36f66bd70d
cbrow353 <cbrow353@uncc.edu>
2023-01-19 23:33:30 -0500

    Added check folder feature

--- diablo_v1.py
+++ diablo_v1.py
@@ -1,1 +1,30 @@
-print('pp hard')
+import os
+import cv2
+import glob
+import random
+
+def check_folder():
+    print('Looking for folder...')
+    if os.path.exists("content"):
+        print('Folder found.')
+    else:
+        print('Folder not found, creating...')
+        os.mkdir("content")
+        print('Done.')
+
+def display():
+    path = "content"
+    filenames = glob.glob(os.path.join(path, "*"))
+    print(filenames)
+
+    for filename in filenames:
+        print(filename)
+        img = cv2.imread(filename)
+
+        cv2.imshow("Slideshow", img)
+
+        if cv2.waitKey(5000) == ord('q'):
+            return
+
+check_folder()
+display()

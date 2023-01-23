e1b15f02ef03183fd54696b1c0531f0c76c96049
cbrow353 <cbrow353@uncc.edu>
2023-01-22 22:39:28 -0500

    Added video capability

--- diablo_v1.py
+++ diablo_v1.py
@@ -12,23 +12,46 @@ def check_folder():
         os.mkdir("content")
         print('Done.')
 
+def play_video(file):
+    cap = cv.VideoCapture(file)
+
+    while cap.isOpened():
+        ret, frame = cap.read() # If frame is read, ret is true
+
+        if not ret:
+            print('Error: did not receive frame. Exiting...')
+            break
+        cv.imshow('window', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+    return None
+
+def display_img(file):
+    print(file)
+    img = cv.imread(file)
+
+    return img
+
 def display():
+    # Open content folder
     path = "content"
     filenames = glob.glob(os.path.join(path, "*"))
     print(filenames)
 
+    # Create window, set to fullscreen, and keep aspect ration of image.
     cv.namedWindow("window", cv.WINDOW_NORMAL)
     #cv.setWindowProperty("window", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
     cv.setWindowProperty("window", cv.WND_PROP_ASPECT_RATIO, cv.WINDOW_KEEPRATIO)
 
+    # Shuffle files in content
     random.shuffle(filenames)
     for filename in filenames:
-        print(filename)
-        img = cv.imread(filename)
+        if filename.endswith('.MOV'):
+            play_video(filename)
+        
+        cv.imshow("window", display_img(filename))
 
-        cv.imshow("window", img)
-
-        if cv.waitKey(1000) == ord('q'):
+        if cv.waitKey(1000) == ord('q'):# TIME FOR IMAGE *************************
             return
 
 check_folder()

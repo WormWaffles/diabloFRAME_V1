c6c02340a248e4d5cafd05832778b13cab4baf00
cbrow353 <cbrow353@uncc.edu>
2023-01-24 14:55:47 -0500

    Finished video capability

--- diablo_v1.py
+++ diablo_v1.py
@@ -15,77 +15,93 @@ def check_folder():
 
 def play_video(file):
     cap = cv.VideoCapture(file)
+    initial_time = time.time()
+    to_time = time.time()
 
-    # background
+    set_fps = 30 # Set frame rate
+
+    # Variables Used to Calculate FPS
+    prev_frame_time = 0
+    new_frame_time = 0
+
+    # Background var
     bg_made = False
 
     while cap.isOpened():
-        ret, frame = cap.read() # If frame is read, ret is true
-        fps = cap.get(cv.CAP_PROP_FPS)
-        print(fps)
-        cv.VideoCapture(0 + cv.CAP_DSHOW)
+        while_running = time.time() # Keep updating time with each frame
+        new_time = while_running - initial_time # If time taken is 1/fps, then read a frame
 
-        if not bg_made:
-            blur_frame = cv.blur(frame, (frame.shape[0]//10, frame.shape[1]//10))
-            width, height = blur_frame.shape[1], blur_frame.shape[0]
-            width = int(width * 220 // 100)
-            height = int(height * 220 // 100)
-            dim = width, height
-            blur_frame = cv.resize(blur_frame, dim, interpolation = cv.INTER_AREA)
-            crop_width, crop_height = image_to_ratio(width, height)
-            blur_frame = blur_frame[0:crop_height, 0:crop_width]
-            bg_made = True
+        if new_time >= 1 / set_fps:
+            ret, frame = cap.read()
 
-        if not ret:
-            print('Error: did not receive frame. Exiting...')
-            break
+            # make bg image
+            if not bg_made:
+                blur_frame = cv.blur(frame, (frame.shape[0]//10, frame.shape[1]//10))
+                width, height = blur_frame.shape[1], blur_frame.shape[0]
+                width = int(width * 220 // 100)
+                height = int(height * 220 // 100)
+                dim = width, height
+                blur_frame = cv.resize(blur_frame, dim, interpolation = cv.INTER_AREA)
+                crop_width, crop_height = image_to_ratio(width, height)
+                blur_frame = blur_frame[0:crop_height, 0:crop_width]
+                bg_made = True
 
-        # Resize frame to fit properly in bg
-        # Resize original to fit in new
-        img_width, img_height = frame.shape[1], frame.shape[0]
-        if img_width // 9 > img_height // 16:
-            img_width = int(img_width * (blur_frame.shape[0] / img_height))
-            img_height = blur_frame.shape[0]
-        elif img_height // 16 > img_width // 9:
-            img_height = int(img_height * (blur_frame.shape[1] / img_width))
-            img_width = blur_frame.shape[1]
+            if ret:
+                # Calculating True FPS
+                new_frame_time = time.time()
+                fps = 1 / (new_frame_time - prev_frame_time)
+                prev_frame_time = new_frame_time
 
-        frame = cv.resize(frame, (img_width, img_height), interpolation = cv.INTER_AREA)
+                # Resize original to fit in new
+                dst = frame # if its not made by the if statement
+                frame_width, frame_height = frame.shape[1], frame.shape[0]
+                if frame_height // 9 != frame_width // 16:
+                    if frame_width // 9 >= (frame_height // 16) - 10:
+                        frame_width = int(frame_width * (blur_frame.shape[0] / frame_height))
+                        frame_height = blur_frame.shape[0]
+                    elif frame_height // 16 >= frame_width // 9:
+                        frame_height = int(frame_height * (blur_frame.shape[1] / frame_width))
+                        frame_width = blur_frame.shape[1]
+                    frame = cv.resize(frame, (frame_width, frame_height), interpolation = cv.INTER_AREA)
 
-        frame = merge_image(blur_frame, frame, ((crop_width // 2) - (frame.shape[1] // 2)), ((crop_height // 2) - (frame.shape[0] // 2)))
-        cv.imshow('photo_frame', frame)
-        if cv.waitKey(25) == ord('q'):
-           break
+                    # Display original image on top
+                    dst = merge_image(blur_frame, frame, ((crop_width // 2) - (frame.shape[1] // 2)), ((crop_height // 2) - (frame.shape[0] // 2)))
+                
+                cv.imshow('photo_frame', dst)
+                initial_time = while_running # Update the initial time with current time
 
-    return
+            else:
+                total_time_of_video = while_running - to_time # To get the total time of the video
+                print(total_time_of_video)
+                break
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
 
 def display_img(file):
     print(file)
     img = cv.imread(file)
+
     # Blur image for background
     img_bg = cv.blur(img, (img.shape[0]//10, img.shape[1]//10))
 
-    # I am sorry to anyone who has to read this
+    # Set size of bg to 16x9
     width, height = img_bg.shape[1], img_bg.shape[0]
     width = int(width * 220 // 100)
     height = int(height * 220 // 100)
     dim = width, height
-    img_bg = cv.resize(img_bg, dim, interpolation = cv.INTER_AREA) # idk what im doing
-
+    img_bg = cv.resize(img_bg, dim, interpolation = cv.INTER_AREA)
     crop_width, crop_height = image_to_ratio(width, height)
-    #start_crop_width = int((width - crop_width) // 2)
-    #start_crop_height = int((height - crop_height) // 2)
-    img_bg = img_bg[0:crop_height, 0:crop_width] # zeros should be start_crop_*
+    img_bg = img_bg[0:crop_height, 0:crop_width]
 
-    # Resize original to fit in new
+    # Resize original to fit in bg
     img_width, img_height = img.shape[1], img.shape[0]
-    if img_width // 9 > img_height // 16:
+    if img_width // 9 >= img_height // 16:
         img_width = int(img_width * (img_bg.shape[0] / img_height))
         img_height = img_bg.shape[0]
-    elif img_height // 16 > img_width // 9:
+    elif img_height // 16 >= img_width // 9:
         img_height = int(img_height * (img_bg.shape[1] / img_width))
         img_width = img_bg.shape[1]
-
     img = cv.resize(img, (img_width, img_height), interpolation = cv.INTER_AREA)
 
     # Display original image on top
@@ -148,7 +164,6 @@ def merge_image(back, front, x,y):
     
     # replace an area in result with overlay
     result = back.copy()
-    print(f'af: {alpha_front.shape}\nab: {alpha_back.shape}\nfront_cropped: {front_cropped.shape}\nback_cropped: {back_cropped.shape}')
     result[y1:y2, x1:x2, :3] = alpha_front * front_cropped[:,:,:3] + (1-alpha_front) * back_cropped[:,:,:3]
     result[y1:y2, x1:x2, 3:4] = (alpha_front + alpha_back) / (1 + alpha_front*alpha_back) * 255
 
@@ -156,3 +171,4 @@ def merge_image(back, front, x,y):
 
 check_folder()
 display()
+cv.destroyAllWindows()
